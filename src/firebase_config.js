a23c51e85f52d784bce35f1cdb554089a53c65ef
Prakrit M <PrakritManStudio@gmail.com>
2022-12-30 20:00:27 +0700

    เพิ่ม bootstrap css, firebase_config

---
+++ src/firebase_config.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = firebase.initializeApp({
+	apiKey: "AIzaSyCymEuTtYwoTmn6qsH0e5rHFGOMK5OaGrU",
+	authDomain: "majorbooking-da59c.firebaseapp.com",
+	projectId: "majorbooking-da59c",
+	storageBucket: "majorbooking-da59c.appspot.com",
+	messagingSenderId: "758564638388",
+	appId: "1:758564638388:web:94380b5f9e0b357c0391e1",
+});
+
+export default firebaseConfig;
+
